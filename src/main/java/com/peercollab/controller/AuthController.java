178bd7b0b65186ba6a3585d56b086da55f8d8ad3
ManchineniJayendraMadhav <2400031383@kluniversity.in>
2026-04-29 10:05:34 +0530

    Update AuthController.java

--- src/main/java/com/peercollab/controller/AuthController.java
+++ src/main/java/com/peercollab/controller/AuthController.java
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "https://peer-review-frontend-three.vercel.app/", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
